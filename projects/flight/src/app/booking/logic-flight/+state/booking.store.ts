2a1f9b6fe2225423b6152b9aaaab317cc9b987c5
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-13 17:37:21 +0100

    refactor: signal store rx method

--- projects/flight/src/app/booking/logic-flight/+state/booking.store.ts
+++ projects/flight/src/app/booking/logic-flight/+state/booking.store.ts
@@ -1,8 +1,11 @@
-import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
+import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
 import { Flight } from "../model/flight";
 import { computed, inject } from "@angular/core";
 import { FlightFilter } from "../model/flight-filter";
 import { FlightService } from "../data-access/flight.service";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
+import { tapResponse } from "@ngrx/operators";
+import { pipe, switchMap } from "rxjs";
 
 
 export const BookingStore = signalStore(
@@ -34,14 +37,19 @@ export const BookingStore = signalStore(
         store,
         flightService = inject(FlightService)
     ) => ({
-        loadFlights: () => {
-            flightService.find(
-                store.filter.from(),
-                store.filter.to(),
-                store.filter.urgent()
-            ).subscribe(
-                flights => store.setFlights(flights)
+        loadFlights: rxMethod<FlightFilter>(pipe(
+            switchMap(filter => flightService.find(
+                filter.from,
+                filter.to,
+                filter.urgent
+            )),
+            tapResponse(
+                flights => store.setFlights(flights),
+                err => console.error(err)
             )
-        }
+        ))
     })),
+    withHooks(store => ({
+        onInit: () => store.loadFlights(store.filter)
+    }))
 );
--- projects/flight/src/app/booking/logic-flight/+state/facade.ts
+++ projects/flight/src/app/booking/logic-flight/+state/facade.ts
@@ -1,21 +1,17 @@
-import { inject } from "@angular/core"
-import { Store } from "@ngrx/store"
-import { ticketActions } from "./actions";
-import { ticketFeature } from "./reducer";
-import { FlightFilter } from "../model/flight-filter";
+import { inject } from "@angular/core";
+import { Store } from "@ngrx/store";
 import { Flight } from "../model/flight";
+import { FlightFilter } from "../model/flight-filter";
 import { BookingStore } from "./booking.store";
 
 
 export function injectTicketsFacade() {
-  const store = inject(Store);
   const bookingStore = inject(BookingStore);
 
   return {
     flights: bookingStore.flights,
     search: (filter: FlightFilter) => {
       bookingStore.setFilter(filter);
-      bookingStore.loadFlights();
     },
     update: (flight: Flight) => {},
     reset: () => bookingStore.setFlights([])
